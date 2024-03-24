e313eb9b0498d37c707d7556f169295d09178e9d
poswalsameer <poswalsameer2003@gmail.com>
2024-03-24 13:35:30 +0530

    initial setup of the project

---
+++ cleartalk/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+
+      <h1 className="text-xl bg-purple-800 text-white">This is the project</h1>
+    
+    </>
+  );
+}
